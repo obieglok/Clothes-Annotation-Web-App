eaf97b87723cb0bb4075caaa20644ed6012d43f9
SamuelAl <s.alarco@outlook.com>
2021-03-02 09:31:34 +0000

    eliminated unnecessary template elements

--- fano/src/App.js
+++ fano/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FANO</h1>
+      <h3>Work in Progress</h3>
     </div>
   );
 }
