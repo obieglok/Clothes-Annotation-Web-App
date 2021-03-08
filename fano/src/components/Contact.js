d42bdcf2834f7ec4624f142f92aef5b5bd578a2b
obieglok <klaudiaobieglo@hotmail.com>
2021-03-08 22:34:37 +0000

    Contact Page

--- fano/src/components/Contact.js
+++ fano/src/components/Contact.js
@@ -1,9 +1,48 @@
 import React from 'react'
-
 const Contact = () => {
     return (
-        <div>
-            <h1>contact page</h1>
+        <div className="container contact ">
+             <h1 className="header">Contact Us</h1>
+             <div className="row">
+                <div className="col s6" >
+                    <thead>
+                        <tr>
+                            <th> Contact ADAPT Centre</th>
+                        </tr>
+                    </thead>
+                
+                    <table className="centered responsive-table">
+                    <tr>
+                
+                        <td><i className="material-icons centre">phone</i></td>
+                        <td><span className="right">+353 1 896 1797</span> <br></br></td>
+                    </tr>
+                    <tr>
+                        <td ><i className="material-icons centre">email</i></td>
+                        <td><span className="right"> info@adaptcentre.ie</span> </td>
+                    </tr>
+                    </table>
+                    
+                </div>
+                <div className="col s6">
+                    <thead>
+                        <tr>
+                            <th> Contact Trinity College</th>
+                        </tr>
+                    </thead>
+                    <table className="centered responsive-table">
+                    <tr>
+                
+                        <td><i className="material-icons centre">phone</i></td>
+                        <td><span className="right">+353 1 896 1000</span> <br></br></td>
+                    </tr>
+                    <tr>
+                        <td><i className="material-icons centre">email</i></td>
+                        <td><span className="right"> info@trinity.com</span> </td>
+                    </tr>
+                    </table>
+                </div>
+             </div>
         </div>
     )
 }
