a6f8d9c52a765d47696ca5849fbc0b32f7084698
SamuelAl <s.alarco@outlook.com>
2021-03-08 19:17:49 +0000

    function to upload images in zip file

--- fano/functions/index.js
+++ fano/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const unzipper = require('unzipper');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,4 +11,24 @@ const functions = require("firebase-functions");
 //   response.send("Hello from Firebase!");
 // });
 
+exports.unpackZip = functions
+    .runWith({timeoutSeconds: 300})
+    .storage.bucket()
+    .object()
+    .onFinalize(async (obj) => {
+        const file = firebase.storage().bucket(obj.bucket).file(obj.name)
 
+        // only deal with ZIP archives
+        if (!file.name.endsWith('.zip')) return
+
+        await file.createReadStream()
+                  .pipe(unzipper.Parse())
+                  .on('entry', entry => {
+                        const destination = firebase.storage()
+                                                  .bucket()
+                                                  .file(`${file.name.replace('.', '_')}/${entry.path}`)
+                        entry.pipe(destination.createWriteStream())
+                  })
+                  .promise()
+        await file.delete()
+    })
