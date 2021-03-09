b65ae085dc148d0ded6074bf41640dda15e60963
SamuelAl <s.alarco@outlook.com>
2021-03-09 09:26:17 +0000

    Implemented image upload in redux store
    Implemented image upload functionality in imageReducer
    Added to rootReducer

--- fano/src/store/reducers/rootReducer.js
+++ fano/src/store/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import { firebaseReducer } from 'react-redux-firebase'
 import {firestoreReducer} from 'redux-firestore'
 import {authReducer} from './authReducer'
+import {imageReducer} from './imageReducer'
 
 import {combineReducers} from 'redux'
 
 const rootReducer = combineReducers({
     auth: authReducer,
+    image: imageReducer,
     firebase: firebaseReducer,
     firestore: firestoreReducer
 })
