1d1b5b3a6298092b7509a94fcf7353e2650fd2ea
obieglok <klaudiaobieglo@hotmail.com>
2021-03-26 12:13:32 +0000

    Create Files for Admin Dashboard
    This is where all the components for the admin dashboard will be

---
+++ fano/src/components/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <h1>
+            AdminDashboard Page
+            </h1>
+        </div>
+    )
+}
+
+export default AdminDashboard
