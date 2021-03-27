daa1dbd79dd4ff86c35884b87b3be952f9cab1c1
SamuelAl <s.alarco@outlook.com>
2021-03-27 19:31:00 +0000

    Testing of fetchNextImage
    demonstration of fetchNextImage included in Test.js

--- fano/src/components/Test.js
+++ fano/src/components/Test.js
@@ -1,39 +1,53 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { exportAnnotations } from '../store/actions/imageActions'
+import { exportAnnotations, fetchNextImage } from '../store/actions/imageActions'
 
 
 export const Test = (props) => {
-    return ( 
-        <div >
-            <button className = "btn"
-                    onClick = {() => {
+    return (
+        <div>
+            <div className="row" >
+                <button className="btn"
+                    onClick={() => {
                         console.log("clicked")
                         props.exportAnnotations()
-                        } } >
-                export annotation 
-            </button> 
-            {props.annotationsJson && <a
-                className="btn"
-                style={{ margin: 10 }}
-                href={URL.createObjectURL(props.annotationsJson)}
-                download="data.json"
-            >
-            DOWNLOAD DATA AS JSON
+                    }} >
+                    export annotation
+            </button>
+                {props.annotationsJson && <a
+                    className="btn"
+                    style={{ margin: 10 }}
+                    href={URL.createObjectURL(props.annotationsJson)}
+                    download="data.json"
+                >
+                    DOWNLOAD DATA AS JSON
             </a>}
+            </div>
+            <div className="row">
+                <button className="btn"
+                        onClick={() => props.fetchNextImage()}>
+                    Fetch next image
+                </button>
+            </div>
+            <div>
+                <img src={props.fetchedImage && props.fetchedImage.imageUrl} alt=""/>
+            </div>
         </div>
+
     )
 }
 
 const mapStateToProps = (state) => ({
-    annotationsJson: state.image.annotationsJson
+    annotationsJson: state.image.annotationsJson,
+    fetchedImage: state.image.fetchedImage
 })
 
 const mapDispatchToProps = (dispatch) => {
     return {
         exportAnnotations: () => {
-            console.log("clicked")
-            dispatch(exportAnnotations())}
+            dispatch(exportAnnotations())
+        },
+        fetchNextImage: () => dispatch(fetchNextImage())
     }
 }
 
