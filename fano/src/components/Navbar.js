4e5c03a7a11a749ecd60d0c31ec2d3546ea2117a
obieglok <klaudiaobieglo@hotmail.com>
2021-03-30 16:09:05 +0100

    Fix navbar

--- fano/src/components/Navbar.js
+++ fano/src/components/Navbar.js
@@ -4,34 +4,29 @@ import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import {connect} from 'react-redux'
-
 class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <nav  className="blue lighten-3">
-                    <div className="nav-wrapper">
-                        <a href="/" className=" left " id="brand">Fano</a>
-                        <a href="#" data-target="mobile-demo" className=" sidenav-trigger right"><i className=" material-icons">menu</i></a>
-                        <ul className="right hide-on-med-and-down">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
-                            <li><Link to="/dashboard">Dashboard</Link></li>
-                        </ul>
-                    </div>
-                </nav>
-                
-                <ul className="sidenav " id="mobile-demo">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/dashboard">Dashboard</Link></li>
-                </ul>
+    render(){
+        const {auth} = this.props;
+        const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks/>
+            return (
+                <div>
+                    <nav  className="blue lighten-3">
+                        <div className="nav-wrapper">
+                            <a href="/" className=" left " id="brand">Fano</a>
+                            <a href="#" data-target="mobile-demo" className=" sidenav-trigger right"><i className=" material-icons">menu</i></a>
+                            <ul className="right hide-on-med-and-down">
+                                {links}
+                            </ul>
+                        </div>
+                    </nav>
+                    
+                    <ul className="sidenav " id="mobile-demo">
+                        {links}
+                    </ul>
 
-            </div>
+                </div>
 
-        )
+            )
     }
 
     componentDidMount() {
