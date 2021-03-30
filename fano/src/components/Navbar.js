1d1aeefe0cbf8eabf1999c03c7a954c5a5f5c6d0
SamuelAl <s.alarco@outlook.com>
2021-03-30 15:37:28 +0100

    revert 1

--- fano/src/components/Navbar.js
+++ fano/src/components/Navbar.js
@@ -6,41 +6,6 @@ import SignedOutLinks from './SignedOutLinks';
 import {connect} from 'react-redux'
 
 class Navbar extends Component {
-    render(){
-        const {auth} = this.props;
-        const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks/>
-            return (
-                <div>
-                    <nav  className="blue lighten-3">
-                        <div className="nav-wrapper">
-                            <a href="/" className=" left " id="brand">Fano</a>
-                            <a href="#" data-target="mobile-demo" className=" sidenav-trigger right"><i className=" material-icons">menu</i></a>
-                            <ul className="right hide-on-med-and-down">
-                                {links}
-                            </ul>
-                        </div>
-                    </nav>
-                    
-                    <ul className="sidenav " id="mobile-demo">
-                        {links}
-                    </ul>
-
-                </div>
-
-            )
-    }
-    componentDidMount() {
-        let sidenav = document.querySelector('#mobile-demo');
-        M.Sidenav.init(sidenav, {edge:'right'});
-    }
-<<<<<<< Updated upstream
-}
-const mapStateToProps = (state) =>{
-    console.log(state);
-    return{
-        auth: state.firebase.auth
-        
-=======
     render() {
         return (
             <div>
@@ -67,8 +32,19 @@ const mapStateToProps = (state) =>{
             </div>
 
         )
->>>>>>> Stashed changes
+    }
+
+    componentDidMount() {
+        let sidenav = document.querySelector('#mobile-demo');
+        M.Sidenav.init(sidenav, {edge:'right'});
     }
 }
+const mapStateToProps = (state) =>{
+    console.log(state);
+    return{
+        auth: state.firebase.auth
+    }
+}
+   
 export default connect(mapStateToProps)(Navbar);
 
