266ff9c1e4aff21a33552f465c8f8ff4d9bd38ca
Iraklis Bogiatziou <60186495+iamiraklis@users.noreply.github.com>
2021-03-30 17:25:41 +0300

    recreate app.js

---
+++ fano/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Navbar from './components/Navbar.js';
+import {BrowserRouter, Route ,Switch } from 'react-router-dom';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import SignIn from './components/SignIn';
+import Register from './components/Register';
+import  Test  from './components/Test';
+import UploadFile from './components/AdminDashboard/UploadFile';
+import AdminDashboard from './components/AdminDashboard/AdminDashboard';
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <Navbar />
+      <Switch>
+        <Route path='/signin' component ={SignIn} />
+        <Route path='/register' component ={Register} />
+        <Route path='/about' component ={About} />
+        <Route path='/contact' component ={Contact} />
+        <Route path='/dashboard' component ={AdminDashboard} />
+        <Route exact path='/uploadFile' component ={UploadFile} />
+        <Route exact path='/' component ={Home} />
+      </Switch>
+      <Footer/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
