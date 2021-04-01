a30ce93a8c760cbb75cba3df0f182804d00eb875
SamuelAl <s.alarco@outlook.com>
2021-04-01 22:08:19 +0100

    fix footer

--- fano/src/App.js
+++ fano/src/App.js
@@ -1,37 +1,45 @@
 
 import './App.css';
 import Navbar from './components/Navbar.js';
-import {BrowserRouter, Route ,Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import SignIn from './components/SignIn';
 import Register from './components/Register';
-import Test  from './components/Test';
+import Test from './components/Test';
 import UploadFile from './components/AdminDashboard/UploadFile';
 import AdminDashboard from './components/AdminDashboard/AdminDashboard';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-      <Navbar />
-      <Switch>
-        <Route path='/signin' component ={SignIn} />
-        <Route path='/register' component ={Register} />
-        <Route path='/about' component ={About} />
-        <Route path='/contact' component ={Contact} />
-        <Route path='/dashboard' component ={AdminDashboard} />
-        <Route path='/test' component ={Test} />
-        <Route exact path='/' component ={Home} />
-        <Route exact path='/uploadFile' component ={UploadFile} />
-        <Route exact path='/adminDash' component ={AdminDashboard} />
-      </Switch>
-      <Footer/>
+    [
+      <header></header>,
+    <main>
+      <BrowserRouter>
+        <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path='/signin' component={SignIn} />
+          <Route path='/register' component={Register} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/dashboard' component={AdminDashboard} />
+          <Route path='/test' component={Test} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/uploadFile' component={UploadFile} />
+          <Route exact path='/adminDash' component={AdminDashboard} />
+        </Switch>
       </div>
-    </BrowserRouter>
+      </BrowserRouter>
+      </main >,
+    <footer class="page-footer blue lighten-3">
+      <Footer />
+    </footer>
+    ]
+    
   );
 }
 
