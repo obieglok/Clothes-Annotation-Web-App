1fd276c3c7dbee28145348ed279a0e8c86372606
Iraklis Bogiatziou <60186495+iamiraklis@users.noreply.github.com>
2021-04-02 21:36:32 +0300

    register signin UI

--- fano/src/components/Register.js
+++ fano/src/components/Register.js
@@ -1,103 +1,141 @@
-import react from 'react';
+import react from "react";
 
-import { grid } from '@material-ui/core';
-import {connect} from 'react-redux'
-import {signUp} from '../store/actions/authActions'
-import { Redirect } from 'react-router-dom'
+import { grid } from "@material-ui/core";
+import { connect } from "react-redux";
+import { signUp } from "../store/actions/authActions";
+import { Redirect } from "react-router-dom";
 
-class Register extends react.Component{
-state={
-    email:'',
-    password:''
-}
+class Register extends react.Component {
+  state = {
+    email: "",
+    password: "",
+  };
 
-handleChange = (e) => {
+  handleChange = (e) => {
     this.setState({
-        [e.target.id]: e.target.value
-    })
-}
+      [e.target.id]: e.target.value,
+    });
+  };
 
-handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(this.state)
-    this.props.signUp(this.state)
-}
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.signUp(this.state);
+  };
 
-
-render(){
-    const {authError, auth} = this.props
+  render() {
+    const { authError, auth } = this.props;
     if (auth.uid) {
-        return <Redirect to='/' />
+      return <Redirect to="/" />;
     }
-    return(
-            <container>
-
-                <div class='fano-text'>
-                    <h1>Annotate clothes.
-                        bring them to life
-                    </h1>
-                </div>
-            <div class='lgn details register'>
-                <div class="lgn-inner ">
-                    <form class='frm1' onSubmit={this.handleSubmit}>
-                        <div class='form-row'>
-                            <input type='text' class='form-c' name='firstName' id="firstName"
-                            placeholder='first name' required onChange={this.handleChange}/>
-                        </div>
-                        <div class='form-row'>
-                            <input type='text' class='form-c'name='lastName' id="lastName"
-                            placeholder='last name' required onChange={this.handleChange}/>
-                        </div>
-                        <div class='form-row'>
-                            <input type='email' class='form-c' name='email' id="email"
-                            placeholder='email' required onChange={this.handleChange}/>
-                        </div>
-                        <div class='form-row'>
-                            <input type='password' class='form-c'name='password' id="password"
-                            placeholder='enter a password' required onChange={this.handleChange}/>
-                        </div>
-                        <div class='form-row'>
-                            <input type='password' class='form-c' name='password'
-                            placeholder='re enter password' required onChange={this.handleChange}/>
-                        </div>
-                        
-                        <div class='form-row-btn'>
-                            <div class='lgn-c'>
-                                <button submit={this.handleSubmit} class='lgn-btn'
-                                > Create an account</button>
-                            </div>
-
-                            <div className="col s12 red-text center">
-                                {authError && <p>Login error. Please check your details</p> }
-                            </div>
-                            
-                        </div>
-                    </form>
-                </div>
-            </div>
-    </container>
-         )
-        }
+    return (
+        <div className="body-sign-in-register">
+            
+      <container>
         
-    }
+        <div class="lgn details register">
+          <div class="lgn-inner ">
+            <form class="frm1" onSubmit={this.handleSubmit}>
+              <div class="form-row">
+                <input
+                  type="text"
+                  class="form-c"
+                  name="firstName"
+                  id="firstName"
+                  placeholder="First Name"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div class="form-row">
+                <input
+                  type="text"
+                  class="form-c"
+                  name="lastName"
+                  id="lastName"
+                  placeholder="Last Name"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div class="form-row">
+                <input
+                  type="email"
+                  class="form-c"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div class="form-row">
+                <input
+                  type="password"
+                  class="form-c"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div class="form-row">
+                <input
+                  type="password"
+                  class="form-c"
+                  name="password"
+                  placeholder="Re-enter password"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
 
-    const mapStateToProps = (state) => {
-        return {
-            authError: state.auth.authError,
-            auth: state.firebase.auth
-        }
-    }
-    
-    const mapDispatchToProps = (dispatch) => {
-        return {
-            signUp: (creds) => dispatch(signUp(creds))
-        }
-    }
-    
-    export default connect(mapStateToProps, mapDispatchToProps)(Register)
-    
-  
-    {/* <label for="fname">First name:</label><br/>
+              <div class="form-row-btn">
+                <div class="lgn-c">
+                  <button submit={this.handleSubmit} className="btn waves-effect waves-light blue accent-4 blockButton">
+                    {" "}
+                    Register
+                  </button>
+                </div>
+
+                <div className="col s12 red-text center">
+                  {authError && <p>Login error. Please check your details</p>}
+                </div>
+                <div className="regis">
+                  <span>Already have an account? </span>
+                  <a href="/signIn">
+                    <span className="redirect">log in here.</span>
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </container>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError,
+    auth: state.firebase.auth,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (creds) => dispatch(signUp(creds)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
+
+{
+  /* <label for="fname">First name:</label><br/>
      <input type="text" id="fname" name="fname"/><br/>
      <label for="lname">Last name:</label><br/>
-     <input type="text" id="lname" name="lname"/> */}
+     <input type="text" id="lname" name="lname"/> */
+}
--- fano/src/components/SignIn.js
+++ fano/src/components/SignIn.js
@@ -1,89 +1,172 @@
-import react from 'react';
-import { grid } from '@material-ui/core';
-import {connect} from 'react-redux'
-import {signIn} from '../store/actions/authActions'
-import { Redirect } from 'react-router-dom'
-
-
-
+import react from "react";
+import { grid } from "@material-ui/core";
+import { connect } from "react-redux";
+import { signIn } from "../store/actions/authActions";
+import { Redirect } from "react-router-dom";
 
 class SignIn extends react.Component {
+  state = {
+    email: "",
+    password: "",
+  };
 
-    state = {
-        email:'',
-        password:''
-
-    }
-
-handleChange = (e) => {
+  handleChange = (e) => {
     this.setState({
-        [e.target.id]: e.target.value
-    })
-}
+      [e.target.id]: e.target.value,
+    });
+  };
 
-handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.signIn(this.state)
- 
-}
-    render(){
-        const {authError, auth} = this.props
-        if (auth.uid) {
-            return <Redirect to='/' />
-        }
-        return(
-            <container className='main-c'>
-                    <div className='fano-text'>
-                        <h1>Annotate clothes.
-                            bring them to life</h1>
-                    </div>
-                    <div className='lgn details'>
-                        <div className="lgn-inner ">
-                            <form className='frm1' onSubmit={this.handleSubmit}>
-                                <div className='form-row'>
-                                    <input type='email' className='form-c' name='email' id='email'
-                                    placeholder='email' required onChange={this.handleChange}/>
-                                </div>
-                                <div className='form-row'>
-                                    <input type='password' className='form-c'name='password' id='password'
-                                    placeholder='password' required onChange={this.handleChange}/>
-                                </div>
-                                <div className='form-row-btn'>
-                                    <div className='lgn-c'>
-                                        <button  className='btn waves-effect waves-light blue accent-4'> Login </button>
-                                    </div>
-                                </div>
-                           
-                            <div className="lgn-row-logo">
-                               
-                                <div className="col s12 red-text center">
-                                {authError && <p>Login error. Please check your details</p> }
-                                </div>
-                                <div className="regis">
-                                    <span>Don't have an account? </span>
-                                    <a href='/register'><span>sign up here.</span></a>
-                                </div>
-                            </div>
-                            </form>
-                        </div>
-                </div>
-            </container>
-         )
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signIn(this.state);
+  };
+  render() {
+    const { authError, auth } = this.props;
+    if (auth.uid) {
+      return <Redirect to="/" />;
     }
+    return (
+<<<<<<< Updated upstream
+      <div className="body-sign-in-register">
 
+      <container className="main-c">
+        
+        <div className="lgn details">
+          <div className="lgn-inner ">
+            <form className="frm1" onSubmit={this.handleSubmit}>
+              <div className="form-row">
+                <input
+                  type="email"
+                  className="form-c"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-row">
+                <input
+                  type="password"
+                  className="form-c"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-row-btn">
+                <div className="lgn-c">
+                  <button className="btn waves-effect waves-light blue accent-4 blockButton">
+                    {" "}
+                    Login{" "}
+                  </button>
+                </div>
+              </div>
+
+              <div className="lgn-row-logo">
+                <div className="col s12 red-text center">
+                  {authError && <p>Login error. Please check your details</p>}
+                </div>
+                <div className="regis">
+                  <span>Don't have an account? </span>
+                  <a href="/register">
+                    <span className="redirect">sign up here.</span>
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </container>
+=======
+      <div className="body">
+        <container className="main-c">
+          
+            <div className="lgn-inner ">
+              <form className="frm1" onSubmit={this.handleSubmit}>
+                <div className="form-row">
+                  <input
+                    type="email"
+                    className="form-c"
+                    name="email"
+                    id="email"
+                    placeholder="email"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-row">
+                  <input
+                    type="password"
+                    className="form-c"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-row-btn">
+                  <div className="lgn-c">
+                    <button className="lgn-btn"> Login </button>
+                  </div>
+                </div>
+                <div className="lgn-row-2">
+                  <div className="sid">
+                    <span>Or continue with</span>
+                  </div>
+                </div>
+                <div className="lgn-row-logo">
+                  <div className="app-logos">
+                    <a href="#">
+                      <div className="app-lg a">
+                        <span>a</span>
+                      </div>
+                    </a>
+                    <a href="#">
+                      <div className="app-lg a">
+                        <span>a</span>
+                      </div>
+                    </a>
+                    <a href="#">
+                      <div className="app-lg a">
+                        <span>a</span>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="col s12 red-text center">
+                    {authError && <p>Login error. Please check your details</p>}
+                  </div>
+                  <div className="regis">
+                    <span>Don't have an account? </span>
+                    <a href="/register">
+                      <span>sign up here.</span>
+                    </a>
+                  </div>
+                </div>
+              </form>
+            
+          </div>
+        </container>
+>>>>>>> Stashed changes
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        authError: state.auth.authError,
-        auth: state.firebase.auth
-    }
-}
+  return {
+    authError: state.auth.authError,
+    auth: state.firebase.auth,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        signIn: (creds) => dispatch(signIn(creds))
-    }
-}
+  return {
+    signIn: (creds) => dispatch(signIn(creds)),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
