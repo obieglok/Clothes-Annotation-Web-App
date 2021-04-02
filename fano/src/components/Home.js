1f9bd1fba70c7044e9161f8230c6c110d9797ab2
obieglok <klaudiaobieglo@hotmail.com>
2021-04-02 11:32:06 +0100

    Update Home page
    Not logged in users can't see home page, will be redirected to sign in page

--- fano/src/components/Home.js
+++ fano/src/components/Home.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import UserProfile from './UserProfile'
-
-const Home = () => {
+import {signIn} from '../store/actions/imageActions'
+import {connect} from 'react-redux'
+import { Redirect } from 'react-router-dom'
+const Home = (props) => {
+    const {authError, auth} = props
+    if (!auth.uid) {
+        return <Redirect to='/signIn' />
+    }
     return (
         <div>
             <UserProfile/>
         </div>
     )
 }
+const mapStateToProps = (state) => {
+    return {
+        authError: state.auth.authError,
+        auth: state.firebase.auth
+    }
+}
 
-export default Home
+export default connect(mapStateToProps)( Home)
