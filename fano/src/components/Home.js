8e7d5ed683b1087f94dada3e9005ee8d13346e23
obieglok <klaudiaobieglo@hotmail.com>
2021-04-02 11:32:53 +0100

    remove not needed lines

--- fano/src/components/Home.js
+++ fano/src/components/Home.js
@@ -4,7 +4,7 @@ import {signIn} from '../store/actions/imageActions'
 import {connect} from 'react-redux'
 import { Redirect } from 'react-router-dom'
 const Home = (props) => {
-    const {authError, auth} = props
+    const {auth} = props
     if (!auth.uid) {
         return <Redirect to='/signIn' />
     }
@@ -16,7 +16,6 @@ const Home = (props) => {
 }
 const mapStateToProps = (state) => {
     return {
-        authError: state.auth.authError,
         auth: state.firebase.auth
     }
 }
