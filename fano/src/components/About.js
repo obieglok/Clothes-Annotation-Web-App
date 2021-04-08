7fbd60b1347d16c87508528f77a9defd5d6361b4
obieglok <klaudiaobieglo@hotmail.com>
2021-04-08 20:51:25 +0100

    Updated about us

--- fano/src/components/About.js
+++ fano/src/components/About.js
@@ -7,9 +7,11 @@ const About = () => {
       <div class="row cardsAboutContact ">
         <div class="col s12 m12">
           <div className="card cardSettings">
-            <h5>What is Fano?</h5>
+            <h5>About</h5>
+            <br></br>
+            <p>Fano challenges its users to read complex text printed on clothes. The better they read, the higher they score.</p>
             <p>
-            This app is co-funded by the European Union’s Horizon 2020 programme under the Marie Skłodowska-Curie Grant Agreement No. 801522 at the ADAPT SFI Research Centre at Trinity College Dublin, as part of the ELITE-S project. ADAPT SFI is funded by Science Foundation Ireland and is co-funded under the European Regional Development Fund (ERDF) through Grant 13/RC/2106_P2.
+            Fano is co-funded by the European Union’s Horizon 2020 programme under the Marie Skłodowska-Curie Grant Agreement No. 801522 at the ADAPT SFI Research Centre at Trinity College Dublin, as part of the ELITE-S project. ADAPT SFI is funded by Science Foundation Ireland and is co-funded under the European Regional Development Fund (ERDF) through Grant 13/RC/2106_P2.
             </p>
 
           </div>
