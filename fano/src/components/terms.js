d9ada8976322e5da773542a525b8302c5609eadb
Terlo <57776535+Terlo@users.noreply.github.com>
2021-04-16 12:54:22 +0100

    addition of Privacy Policy & Terms page
    Creation of a Privacy policy page
    Creation of a terms page
    addition of notice of acknowledgment of these two pages on the register page.

---
+++ fano/src/components/terms.js
@@ -0,0 +1,170 @@
+import react from "react";
+
+
+class Terms extends react.Component {
+
+
+state ={
+  isLegalAge:false
+};
+
+onChange = e=>{
+  this.setState({isLegalAge:e.target.checked})
+}
+  render() {
+
+    const {isLegalAge} =this.state;
+
+    return (
+      <div className="body-sign-in-register">
+        <container className="register">
+        <div class="lgn details">
+        <div class="terms policy">
+        <h1>END USER LICENSE AGREEMENT</h1>
+        <p>Last updated 16/04/2021.<br/>
+        fano is licensed to You (End-User) by fano for use only under the terms of this License Agreement.<br/>
+        By using the application and services and any update thereto (as permitted by this License Agreement),
+        You indicate that You agree to be bound by all of the terms and conditions of this License Agreement, and that You accept this License Agreement.
+        </p>
+        <h1>
+        1. THE APPLICATION FANO 
+        </h1>
+        <p>
+        (hereinafter: Application) is a piece of software created to populate image data with relevant data via user contibuted annotations.<br/>
+        </p>
+
+        <h1>
+        2. NO MAINTENANCE OR SUPPORT<br/>
+        </h1>
+        Fano is not obligated, expressed or implied, to provide any maintenance, technical or other support for the Application. <br/>
+
+        <h1>
+        3. USER GENERATED CONTRIBUTIONS
+        </h1>
+        <p>
+        By contributing to Fano You represent and warrant that:<br/>
+        3.1 Your Contributions are not false, inaccurate, or misleading. <br/>
+        3.2 Your Contributions are not unsolicited or unauthorized advertising, promotional materials, pyramid schemes, chain letters, spam, mass mailings, or other forms of solicitation. <br/>
+        3.3 Your Contributions are not obscene, lewd, lascivious, filthy, violent, harassing, libelous, slanderous, or otherwise objectionable (as determined by the Republic of Ireland). <br/>
+        3.4 Your Contributions do not ridicule, mock, disparage, intimidate, or abuse anyone. <br/>
+        3.5 Your Contributions are not used to harass or threaten (in the legal sense of those terms) any other person and to promote violence against a specific person or class of people. <br/>
+        3.6 Your Contributions do not violate any applicable law, regulation, or rule. <br/>
+        3.7 Your Contributions do not violate the privacy or publicity rights of any third party. <br/>
+        3.8 Your Contributions do not contain any material that solicits personal information from anyone under
+        the age of 18 or exploits people under the age of 18 in a sexual or violent manner. <br/>
+        3.9 Your Contributions do not violate any applicable law concerning child pornography, or otherwise
+        intended to protect the health or well-being of minors. <br/>
+        3.10 Your Contributions do not include any offensive comments that are connected to race, national origin, gender, sexual preference, or physical handicap. <br/>
+        3.11 Your Contributions do not otherwise violate, or link to material that violates, any provision of these Terms of Use, or any applicable law or regulation. <br/>
+        Any use of the Application
+        in violation of the foregoing violates these Terms of Use and may result in, among other things, termination or suspension of your rights to use the Application.<br/>
+        
+        </p>
+        
+        <h1>
+        4. CONTRIBUTION LICENSE
+        </h1>
+
+        You agree that we may access, store, process, and use any information and personal data that you
+        provide following the terms of the Privacy Policy. By submitting suggestions of other feedback regarding the Application, you agree that we can use and
+        share such feedback for any purpose without compensation to you. <br/>
+        You also represent and warrant that:<br/>
+        4.1 We assert full ownership over your Contributions. <br/>
+        4.2 You retain no ownership of any of your contributions and forfeit any intellectual property rights or other proprietary rights associated with your Contributions.<br/>
+        4.3 you expressly agree to exonerate us from any and all responsibility and to refrain from any legal action against us regarding your Contributions. <br/>
+
+
+        <h1>
+        5. WARRANTY 
+        </h1>
+        5.1 Licensor warrants that the Application is free of spyware, trojan horses, viruses, or any other malware.<br/>
+        Licensor warrants that the Application works as described in the user documentation. <br/>
+        5.2 No warranty is provided for the Application that is not executable on the device, that has been
+        unauthorizedly modified, handled inappropriately or culpably, combined or installed with inappropriate
+        hardware or software, used with inappropriate accessories, regardless if by Yourself or by third parties, or
+        if there are any other reasons outside of Fano's sphere of influence that affect the executability of
+        the Application. <br/>
+        5.3 You are required to inspect the Application immediately after creating an account and notify Fano about
+        issues discovered without delay by e-mail or relevant contact information provided on contact page of Fano. The defect report will be taken into
+        consideration and further investigated if it has been mailed within a period of 7 days after discovery. <br/>
+        5.4 If we confirm that the Application is defective, Fano reserves a choice to remedy the situation
+        either by means of solving the defect or substitute delivery. <br/>
+
+
+
+        <h1>
+        6. PRODUCT CLAIMS
+        </h1>
+        <p>
+        Fano and the End-User acknowledge that Fano is responsible for addressing any claims of the End-User or any third party relating to the licensed Application or the End-User’s possession and/or use of that licensed Application, including, but not limited to: <br/>
+        - product liability claims <br/>
+        - any claim that the licensed Application fails to conform to any applicable legal or regulatory requirement <br/>
+        </p>
+
+
+
+        <h1>
+        7. LEGAL COMPLIANCE
+        </h1>
+        <p>
+        You represent and warrant that You are not listed on any Irish Government list of prohibited or restricted parties. <br/>
+        </p>
+
+        <h1>
+        8. CONTACT INFORMATION
+        </h1>
+        <p>
+        For general inquiries, complaints, questions or claims concerning the licensed Application, please contact us via the relevant contact information provided on the contact page<br/>
+        </p>
+
+
+        <h1>
+        9. TERMINATION
+        </h1>
+        <p>
+        The license is valid until terminated by Fano or by You. Your rights under this license will terminate
+        automatically and without notice from Fano if You fail to adhere to any term(s) of this license.<br/> 
+        Upon License termination, You shall stop all use of the Application. <br/>
+        </p>
+
+
+        <h1>
+        10. APPLICABLE LAW
+        </h1>
+        <p>
+
+        This license agreement is governed by the laws of the republic of Ireland excluding its conflicts of law rules.
+        </p>
+
+        <h1>
+        11. MISCELLANEOUS
+        </h1>
+        <p>
+        11.1 If any of the terms of this agreement should be or become invalid, the validity of the remaining
+        provisions shall not be affected. <br/>
+        11.2 Invalid terms will be replaced by valid ones formulated in a way that will achieve the primary purpose.<br/>
+        11.3 Collateral agreements, changes and amendments are only valid if laid down in writing.<br/>
+        11.4 The preceding clause can only be waived in writing<br/>
+        </p>
+                
+        <h1>12. AGE REQUIREMENTS</h1>
+        <p>
+          12.1 If you’re under age 18, which is the required age to manage and own a Fano account, you are not permitted
+          under any circumstance to own a fano account.
+          12.2 If you’re a parent or legal guardian, and you allow your child to use the services on your behalf, then these terms
+          apply to you and you assume complete responsibiliy for your child’s activity on the services.
+        </p>
+               
+      </div>
+      </div>
+      </container>
+      </div>
+    );
+  }
+}
+
+
+
+export default Terms;
+
+
